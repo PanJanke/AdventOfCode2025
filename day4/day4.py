685ed6883cbe13627009ccda43e3774bba0a1efd
Pan_Janke <jan.krol.legowski@gmail.com>
2025-12-11 10:56:22 +0100

    day4 - both solutions

--- day4/day4.py
+++ day4/day4.py
@@ -1,4 +1,4 @@
-with open("../data/test.txt", "r") as f:
+with open("../data/test2.txt", "r") as f:
     grid = [list(line.strip()) for line in f]
 
 
@@ -12,20 +12,37 @@ dirs = [(-1,-1), (-1,0), (-1,1),
         (1,-1),  (1,0), (1,1)]
 
 adj_count = [[0]*cols for _ in range(rows)]  # same shape as grid
+counter=0
+sum_of_total = 0
+while True:
+    for y in range(cols):
+        for x in range(rows):
+            if grid[y][x] == '@':
+                count = 0
+                for dr, dc in dirs:
+                    new_y, new_x = y + dr, x + dc
+                    if 0 <= new_y < rows and 0 <= new_x < cols:
+                        if grid[new_y][new_x] == '@':
+                            count += 1
+                adj_count[y][x] = count
 
-for y in range(rows):
-    for x in range(cols):
-        if grid[y][x] == '@':
-            count = 0
-            for dr, dc in dirs:
-                new_y, new_x = y + dr, x + dc
-                if 0 <= new_y < rows and 0 <= new_x < cols:
-                    if grid[new_y][new_x] == '@':
-                        count += 1
-            adj_count[y][x] = count
+    total = 0
+    for y in range(cols):
+        for x in range(rows):
+            if grid[y][x] == '@' and adj_count[y][x] < 4:
+                grid[y][x] = 'x'
+                total += 1
+            elif grid[y][x] == 'x':
+                grid[y][x] = '.'
 
-for row in adj_count:
-    print(row)
+    if total == 0:
+        break
 
-total = sum(1 for row in adj_count for v in row if (0 < v < 4))
-print(total)
+    sum_of_total += total
+    print(f"======total: {total}")
+    #for row in grid:
+        #print(row)
+
+
+
+print(sum_of_total)
